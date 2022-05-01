2ede496a6f05885104a8790b819070785c2ce649
詹题斯 <focustice@outlook.com>
2022-05-01 23:53:44 +0800

    项目初始化

---
+++ src/main/java/com/tice/tcfun/TcfunApplication.java
@@ -0,0 +1,13 @@
+package com.tice.tcfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcfunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcfunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tice/tcfun/TcfunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tice.tcfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcfunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
